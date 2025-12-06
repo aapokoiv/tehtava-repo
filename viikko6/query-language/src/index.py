c683862df1b859d4fe71d6d5f17b0724ce603f2b
aapokoiv <koivula.aapo@gmail.com>
2025-12-07 00:23:11 +0200

    NHL-reader tasks done: vk6 3rd & 4th exercise done

--- viikko6/query-language/src/index.py
+++ viikko6/query-language/src/index.py
@@ -1,16 +1,17 @@
 from statistics import Statistics
+
+from matchers import All, And, HasAtLeast, HasFewerThan, Not, Or, PlaysIn
 from player_reader import PlayerReader
-from matchers import And, HasAtLeast, PlaysIn
+
 
 def main():
     url = "https://studies.cs.helsinki.fi/nhlstats/2024-25/players.txt"
     reader = PlayerReader(url)
     stats = Statistics(reader)
 
-    matcher = And(
-        HasAtLeast(5, "goals"),
-        HasAtLeast(20, "assists"),
-        PlaysIn("PHI")
+    matcher = Or(
+        HasAtLeast(45, "goals"),
+        HasAtLeast(70, "assists")
     )
 
     for player in stats.matches(matcher):
