a76171842bdc780d39f557f53cf03934bc0be9d7
aapokoiv <koivula.aapo@gmail.com>
2025-12-13 18:24:34 +0200

    Small bugfix

--- viikko7/kivi-paperi-sakset/src/index.py
+++ viikko7/kivi-paperi-sakset/src/index.py
@@ -17,21 +17,21 @@ def main():
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            kaksinpeli = luo_peli(vastaus)
+            kaksinpeli = luo_peli("a")
             kaksinpeli.pelaa()
         elif vastaus.endswith("b"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            yksinpeli = luo_peli(vastaus)
+            yksinpeli = luo_peli("b")
             yksinpeli.pelaa()
         elif vastaus.endswith("c"):
             print(
                 "Peli loppuu kun pelaaja antaa virheellisen siirron eli jonkun muun kuin k, p tai s"
             )
 
-            haastava_yksinpeli = luo_peli(vastaus)
+            haastava_yksinpeli = luo_peli("c")
             haastava_yksinpeli.pelaa()
         else:
             break
